9660a8103094898583ff9b34be5c69882b8e5cfb
BrunoSprint <brunosprintdev@outlook.com>
2023-12-20 19:10:42 -0300

    .

---
+++ JavaScript/java.js
@@ -0,0 +1,34 @@
+let carro = ["chevrolet", "dodge", "ford"]
+// No Vetor acima é descrito em coleção, muitiplos valores.
+// Pode ser um let-variavel, ou cons-constantes
+
+// Vetores BiDimencionais ( Matrizes), Colunas&Linhas.
+
+
+// em caso de busca por posição expecifica usar ordem numerica.
+console.log(carro[1])
+
+let carro2 = "dodge"
+let carro3 ="ford"
+// No esquema acima é descrito em Vetores singulares, unico valor.
+
+
+carro.shift()
+// Neste caso elimina-se o primeiro valor da coleção carro.
+carro.pop()
+// Neste caso será removido o ultimo valor da coleção carro.
+
+
+console.log(carro)
+
+
+
+// Método de concatenação JavaScript.
+var string1 = "Olá, ";
+var string2 = "mundo!";
+var resultado = string1 + string2;
+
+console.log(resultado); // Saída: Olá, mundo!
+
+
+
